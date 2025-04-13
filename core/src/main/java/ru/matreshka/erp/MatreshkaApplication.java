73c808f7dadd6548a969a5d12a45312b7d9353cf
Nikolay <tolmachewvalery@gmail.com>
2025-04-13 16:38:29 +0300

    Аdding import

--- core/src/main/java/ru/matreshka/erp/MatreshkaApplication.java
+++ core/src/main/java/ru/matreshka/erp/MatreshkaApplication.java
@@ -1,9 +1,15 @@
+// Для теста
 package ru.matreshka.erp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.*;
+import java.io.File;
+import java.time.LocalDateTime;
+
 @SpringBootApplication
+
 public class MatreshkaApplication {
 
     public static void main(String[] args) {
